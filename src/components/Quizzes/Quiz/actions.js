7f2c716312f0fa3b0476c58008414b21b11e0104
Kendall Wahnschaffe <kpwahn@live.com>
2018-01-05 10:04:44 -0600

    fix: adding tag to last action creator

--- src/components/Quizzes/Quiz/actions.js
+++ src/components/Quizzes/Quiz/actions.js
@@ -1,10 +1,11 @@
 import { createAction, handleAction } from 'redux-actions';
 import axios from 'axios';
+import {url} from '../../../config';
 
 export const GET_QUESTIONS = 'GET_QUESTIONS';
 
 const getQuestionsAction = createAction(GET_QUESTIONS, function(obj) {
-  const request = axios.get(`http://localhost:3000/api/books/${obj.activeBook.id}/quizzes/${obj.activeQuiz.id}/questions/`);
+  const request = axios.get(`${url}/api/books/${obj.activeBook.id}/quizzes/${obj.activeQuiz.id}/questions/`);
   return request;
 });
 
